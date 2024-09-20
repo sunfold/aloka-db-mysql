086b3d33ebe5570fcef181ca619a107ac4c37a6c
John Tasevski <jatasevski@gmail.com>
2024-09-20 16:14:44 +0800

    Reformatted exploratory queries
    Reformatted and condensed exploratory-queries.sql for ease of reading

--- exploratory-queries.sql
+++ exploratory-queries.sql
@@ -1,18 +1,17 @@
 /* Initial exploratory queries to understand more about the data.
-Each section has prompt(s)/question(s), followed by the answer(s), and then the queries themselves */
+Each section has prompt(s)/question(s), followed by the answer(s), and then the queries themselves. */
 
--- Determine how many states have no active customers. 
--- Answer: 31
+
+
+/* Determine how many states have no active customers. ANSWER: 31 */
 SELECT COUNT(state_name)
 FROM states
 WHERE state_id NOT IN 
 	(SELECT state
 	FROM customers);
 
--- ----------------------------------------------------------------------
 
--- Determine which state has the highest count of active customers. 
--- Answer: CA with 6 customers.
+/* Determine which state has the highest count of active customers. ANSWER: CA with 6 customers. */
 SELECT state, 
 	COUNT(state) AS count_state
 FROM customers
@@ -21,11 +20,10 @@ GROUP BY state
 ORDER BY count_state DESC
 LIMIT 1;
 
--- ----------------------------------------------------------------------
 
--- Find the minimum, average rounded to 2 places, and maximum amount in payments.
--- Also find the range, standard deviation, and the count of records in payments.
--- Answer: Min = 567.89, Avg = 2147.79, Max = 4657.89, Range = 4000, Stdev = approx. 976.58, Count = 35
+/* Find the minimum, average rounded to 2 places, and maximum amount in payments.
+Also find the range, standard deviation, and the count of records in payments.
+ANSWER: Min = 567.89, Avg = 2147.79, Max = 4657.89, Range = 4000, Stdev = approx. 976.58, Count = 35 */
 SELECT MIN(amount) as min_amount, 
 	ROUND(AVG(amount), 2) as avg_amount, 
 	MAX(amount) as max_amount,
@@ -34,11 +32,9 @@ SELECT MIN(amount) as min_amount,
 	COUNT(*) as count_payments
 FROM payments;
 
--- ----------------------------------------------------------------------
 
--- Rank the rounded average gross revenue per customer, partitioned by customer state, ordered by average per state descending.
--- Limit to 5 entries.
--- Answer: 1. MD = $3456.78, 2. WA = $3234.56, 3. OR = $2733.96, SC = $2679.01, NV = $2567.89
+/* Rank the rounded average gross revenue per customer, partitioned by customer state, ordered by average per state descending.
+Limit to 5 entries. ANSWER: 1. MD = $3456.78, 2. WA = $3234.56, 3. OR = $2733.96, SC = $2679.01, NV = $2567.89 */
 WITH cavg AS (
 	SELECT customer_id, 
 		AVG(amount) as avg_amount
@@ -62,11 +58,9 @@ FROM savg
 ORDER BY avg_per_state DESC
 LIMIT 5;
 
--- ----------------------------------------------------------------------
 
--- Find the minimum, rounded avg, maximum, and rounded standard deviation for length of stay in hotel_bookings per year.
--- Order by average length of stay descending.
--- Answer (in terms of average stay): 2022 = 11.00 days, 2024 = 10.54 days, 2023 = 9.63 days
+/* Find the minimum, rounded avg, maximum, and rounded standard deviation for length of stay in hotel_bookings per year.
+Order by average length of stay descending. ANSWER (in terms of average stay): 2022 = 11.00 days, 2024 = 10.54 days, 2023 = 9.63 days */
 WITH ddiff AS (
 	SELECT EXTRACT(YEAR FROM check_in_date) AS years,
 		DATEDIFF(check_out_date, check_in_date) AS date_diff
@@ -89,14 +83,10 @@ SELECT years,
 FROM statdiff
 ORDER BY avg_stay DESC;
 
--- ----------------------------------------------------------------------
 
--- Find the total gross revenue per month, per year, for 2023 and 2024.
--- Compare to overall average gross revenue
--- Order by highest gross revenue descending
--- Which month had the highest gross revenue, and what is the overall average gross revenue per month?
--- Answer, part 1: February 2024 had the highest monthly gross revenue at $11,726.14. 
--- Answer, part 2: The overall average gross revenue per month is $6558.01.
+/* Find the total gross revenue per month, per year, for 2023 and 2024. Compare to overall average gross revenue.
+Order by highest gross revenue descending. Which month had the highest gross revenue, and what is the overall average gross revenue per month?
+ANSWER: February 2024 had the highest monthly gross revenue at $11,726.14. The overall average gross revenue per month is $6558.01. */
 WITH dtcte as (
 SELECT SUM(amount) as gross_rev,
 	YEAR(payment_date) AS year,
@@ -114,11 +104,9 @@ FROM dtcte
 WHERE year IN (2023, 2024)
 ORDER BY gross_rev DESC;
 
--- ----------------------------------------------------------------------
 
--- Determine gross revenue for each hotel. Order by total gross revenue ascending.
--- Which hotel has had the lowest gross revenue?
--- Answer: Phoenix Grand Hotel has had the lowest gross revenue of $789.01.
+/* Determine gross revenue for each hotel. Order by total gross revenue ascending. Which hotel has had the lowest gross revenue?
+ANSWER: Phoenix Grand Hotel has had the lowest gross revenue of $789.01. */
 SELECT
 	hotel_name,
 	SUM(amount) as gross_revenue
